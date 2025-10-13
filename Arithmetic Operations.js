cbab51324cf28b38627c398ae7569cca046da1f7
hariharanchukkala <hariharan.chukkala@gmail.com>
2025-10-13 20:40:07 +0530

    Delete Arithmetic Operations.js

--- Arithmetic Operations.js
+++
@@ -1,7 +0,0 @@
-let a = 10;
-let b = 5;
-console.log("Addition:", a + b);
-console.log("Subtraction:", a - b);
-console.log("Multiplication:", a * b);
-console.log("Division:", a / b);
-console.log("Modulus:", a % b);
