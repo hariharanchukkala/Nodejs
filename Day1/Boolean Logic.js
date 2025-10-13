975b8175530c4cb24d07e9ca54a3132bcb877f5d
hariharanchukkala <hariharan.chukkala@gmail.com>
2025-10-13 20:48:47 +0530

    Add files via upload

---
+++ Day1/Boolean Logic.js
@@ -0,0 +1,7 @@
+let x = 4;
+let y = 12;
+console.log("x > y:", x > y);
+console.log("x < y:", x < y);
+console.log("x == 4 AND y == 12:", x == 4 && y == 12);
+console.log("x == 4 OR y == 12:", x == 10 || y == 20);
+console.log("NOT (x == y):", !(x == y));
